26c95d5bee387c484032e0f85cdd0523b942e150
tenko <tajm.ogunbiyi@gmail.com>
2022-08-12 19:29:41 +0100

    :construction_worker: web3.js now working

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import TxDetails from './TxDetails';
+import * as dotenv from 'dotenv';
 
 const SearchBar = () => {
+  const QUICKNODE_API_KEY_URL = process.env.REACT_APP_QUICKNODE_API_KEY_URL;
   const [hash, setHash] = useState('');
 
   const handleSubmit = (e: any) => {
@@ -19,11 +21,13 @@ const SearchBar = () => {
           placeholder="Search by Address / Txn Hash / Block"
           value={hash}
           onChange={(e) => setHash(e.target.value)}
-          className="p-2 w-[50vw] border-2"
+          className="p-2 w-[50vw] border-2 border-black"
         />
-        <button className="p-2 font-bold border-2 border-l-0">Search</button>
+        <button className="p-2 font-bold border-2 border-l-0 border-black">
+          Search
+        </button>
       </form>
-      <TxDetails hash={hash} />
+      <TxDetails nodeKey={QUICKNODE_API_KEY_URL} hash={hash} />
     </>
   );
 };
--- src/components/TxDetails.tsx
+++ src/components/TxDetails.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import Web3 from 'web3';
 
 interface TxDetailsProps {
   hash: string;
+  nodeKey: string | undefined;
 }
 
-const TxDetails: React.FC<TxDetailsProps> = ({ hash }) => {
+const TxDetails: React.FC<TxDetailsProps> = ({ hash, nodeKey }) => {
+  const web3 = new Web3(`${nodeKey}`);
+  const test = () => {
+    web3.eth.getBlockNumber().then((result) => {
+      console.log('Latest Ethereum Block is ', result);
+    });
+  };
+
   return (
     <div>
       <h1>{hash}</h1>
+      <button onClick={test}>Test</button>
     </div>
   );
 };
