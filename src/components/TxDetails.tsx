16d2f432f6900aea261d41c20315c3868b933efd
tenko <tajm.ogunbiyi@gmail.com>
2022-08-14 12:40:45 +0100

    :construction_worker: added block confirmations in transaction details

--- src/components/TxDetails.tsx
+++ src/components/TxDetails.tsx
@@ -10,6 +10,7 @@ interface TxDetailsProps {
 const TxDetails: React.FC<TxDetailsProps> = ({ hash, web3 }) => {
   const [txHash, setTxHash] = useState('');
   const [txBlock, setTxBlock] = useState<number | null>();
+  const [blockConfirmations, setBlockConfirmations] = useState<number>();
   const [txFrom, setTxFrom] = useState<string | null>();
   const [txTo, setTxTo] = useState<string | null>();
   const [txValue, setTxValue] = useState('');
@@ -22,6 +23,7 @@ const TxDetails: React.FC<TxDetailsProps> = ({ hash, web3 }) => {
     // console.log(res);
     setTxHash(res.hash);
     setTxBlock(res.blockNumber);
+
     setTxFrom(res.from);
     setTxTo(res.to);
     setTxValue(web3.utils.fromWei(res.value, 'ether'));
@@ -30,8 +32,10 @@ const TxDetails: React.FC<TxDetailsProps> = ({ hash, web3 }) => {
       web3.utils.fromWei((res.gas * parseInt(res.gasPrice)).toString(), 'ether')
     );
 
-    // @ts-ignore
-    const block = await web3.eth.getBlock(txBlock);
+    const block = await web3.eth.getBlock(txBlock!);
+    const currentBlock = await web3.eth.getBlockNumber();
+    setBlockConfirmations(currentBlock - txBlock!);
+
     const blockTimestamp = block.timestamp;
     // @ts-ignore
     const timestamp = new Date(parseInt(blockTimestamp) * 1000);
@@ -44,7 +48,7 @@ const TxDetails: React.FC<TxDetailsProps> = ({ hash, web3 }) => {
 
   return (
     <div>
-      <h2 className="font-bold text-lg m-4 ml-0">Transaction details</h2>
+      <h2 className="font-bold text-lg m-4 ml-0">Transaction Details</h2>
       <div className="flex-row gap-5 border-2 border-black p-4">
         <div className="flex">
           <div className="w-[10vw]">Transaction Hash:</div>
@@ -52,7 +56,10 @@ const TxDetails: React.FC<TxDetailsProps> = ({ hash, web3 }) => {
         </div>
         <div className="flex">
           <div className="w-[10vw]">Block:</div>
-          {txBlock}
+          <div>{txBlock}</div>
+          <div className="bg-gray-200 ml-4">
+            {blockConfirmations} block confirmations
+          </div>
         </div>
         <div className="flex">
           <div className="w-[10vw]">Timestamp:</div>
