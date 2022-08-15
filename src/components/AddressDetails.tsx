256332bc05f03878275fb8276940d91fe615784c
tenko <tajm.ogunbiyi@gmail.com>
2022-08-15 02:08:14 +0100

    :sparkles: added address search functionality

--- src/components/AddressDetails.tsx
+++ src/components/AddressDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Web3 from 'web3';
 
@@ -6,12 +6,28 @@ interface AddressDetailsProps {
   web3: Web3;
 }
 
-const AddressDetails: React.FC<AddressDetailsProps> = () => {
+const AddressDetails: React.FC<AddressDetailsProps> = ({ web3 }) => {
   const { id } = useParams();
+
+  const [balance, setBalance] = useState('');
+
+  const getAddressDetails = async () => {
+    const res = await web3.eth.getBalance(id!);
+    setBalance(web3.utils.fromWei(res, 'ether'));
+  };
+
+  useEffect(() => {
+    getAddressDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
-      <h1>hello</h1>
+      <div>Account balance: {balance} ETH</div>
+      <div>
+        for more details about this address,{' '}
+        <a href={`https://debank.com/profile/${id}`}>click here.</a>
+      </div>
     </div>
   );
 };
