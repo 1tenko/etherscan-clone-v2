ce6a344de94f9cc80f67dd9a24ed9cb96a64274b
tenko <tajm.ogunbiyi@gmail.com>
2022-08-17 18:24:10 +0100

    :hammer: fixed EBE typo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,7 +17,7 @@ const Nav: React.FC = () => {
   return (
     <div className="absolute h-[94px] w-[70vw] flex justify-between border-b-2 p-[8px]">
       <div className="flex items-center font-bold text-[64px] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
-        <Link to="/">EBL.</Link>
+        <Link to="/">EBE.</Link>
       </div>
       <div className="flex items-center font-bold">
         Current ETH Price is ${ethPrice}
